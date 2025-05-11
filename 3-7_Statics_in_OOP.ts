954c6e6bb161342a5e69fc3e1178971b500de15c
Rubaeid-Sanjid <rubaeid.sanjid@gmail.com>
2025-05-12 01:58:18 +0600

    learn static

---
+++ 3-7_Statics_in_OOP.ts
@@ -0,0 +1,20 @@
+{
+  class Counter {
+    static count: number = 0;
+    increment() {
+      return (Counter.count = Counter.count + 1);
+    }
+    static decrement() {
+      return (Counter.count = Counter.count - 1);
+    }
+  }
+
+  const instance1 = new Counter();
+  console.log(instance1.increment()); // 1
+
+  const instance2 = new Counter();
+  console.log(instance2.increment()); // 2
+
+  console.log(Counter.decrement()); // As decrement is declared static, we can call it directly using class name
+  // 1
+}
