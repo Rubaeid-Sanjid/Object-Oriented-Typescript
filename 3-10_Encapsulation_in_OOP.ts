83b2621aebc03c587c59a5912bcd805e19e398f8
Rubaeid-Sanjid <rubaeid.sanjid@gmail.com>
2025-05-12 21:01:33 +0600

    learn encapsulation

---
+++ 3-10_Encapsulation_in_OOP.ts
@@ -0,0 +1,35 @@
+{
+    {
+    class BankAccount{
+        readonly id: number;
+        public name: string;
+        // private _balance : number    // Encapsulate
+        protected _balance : number     // Encapsulate
+
+        constructor(id : number, name : string, balance : number){
+            this.id = id;
+            this.name = name;
+            this._balance = balance;
+        }
+
+        addDeposit(amount : number){
+            this._balance = this._balance + amount;
+        }
+        getBalance(){
+            return this._balance;
+        }
+
+    }
+
+    const user = new BankAccount(111, "Sanjid", 1000);
+    user.addDeposit(500);
+    const myBalance = user.getBalance();
+    console.log(myBalance);
+    
+    class StudentAccount extends BankAccount{
+        test(){
+            this._balance;
+        }
+    }
+}
+}
